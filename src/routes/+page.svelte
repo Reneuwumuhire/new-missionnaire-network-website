d3bf50a6bc21b5f7922f2b7f84804696c6eadad4
ninijosue <ninijosue123@gmail.com>
2023-08-03 16:45:32 +0200

    remove comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,6 @@
 	import en from '../translations/en';
 	const languages = { en, fr };
 	dict.set(languages);
-	// locale.set("fr")
 	const webName: string = 'missionaire network website';
 	const name = 'website';
 	const email = 'site@gmail.com';
@@ -20,7 +19,10 @@
 </select>
 
 <h4 class="text-center text-7xl pt-10 px-10 mb-10">{$t('welcome')}</h4>
-<h1 class="text-center text-7xl pt-10 px-10 mb-10 text-gray-500">{$t("This is the implementation for")} {webName}</h1>
+<h1 class="text-center text-7xl pt-10 px-10 mb-10 text-gray-500">
+	{$t('This is the implementation for')}
+	{webName}
+</h1>
 <ul class="divide-y grid items-center justify-center divide-gray-200">
 	<li class="py-4 flex mt-[100px]">
 		<div class="ml-3 grid gap-10">
