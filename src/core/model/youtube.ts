2fc36ab9f6e01e9fb46987af71155d4bef16f6b0
Justin Bahati <bajustone@gmail.com>
2023-08-03 09:02:59 +0200

    adding /api/recent-videos api

---
+++ src/core/model/youtube.ts
@@ -0,0 +1,47 @@
+import z from "zod";
+
+
+export const YTVideoThumbNailSchema = z.object({
+    url: z.string().url(),
+    width: z.number(),
+    height: z.number()
+});
+export type YoutubeVideoThumbNail = z.infer<typeof YTVideoThumbNailSchema>;
+export const YTVideoResourceSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string(),
+    publishedAt: z.coerce.date(),
+    thumbNails: z.object({
+        default: YTVideoThumbNailSchema,
+        medium: YTVideoThumbNailSchema,
+        high: YTVideoThumbNailSchema,
+    })
+});
+export type YTVideoResource = z.infer<typeof YTVideoResourceSchema>
+
+export const YtSearchResultSchema = z.object({
+    pageInfo: z.object({
+        totalResults: z.number(),
+        resultsPerPage: z.number()
+    }),
+    nextPageToken: z.string(),
+    items: z.object({
+        id: z.object({
+            kind: z.string(),
+            videoId: z.string()
+        }),
+        snippet: z.object({
+            publishedAt: z.coerce.date(),
+            channelId: z.string(),
+            title: z.string(),
+            description: z.string(),
+            thumbnails: z.object({
+                default: YTVideoThumbNailSchema,
+                medium: YTVideoThumbNailSchema,
+                high: YTVideoThumbNailSchema,
+            })
+        })
+    }).array()
+});
+export type YtSearchResult = z.infer<typeof YtSearchResultSchema>;
---
+++ src/routes/api/recent-videos/+server.ts
@@ -0,0 +1,39 @@
+import { json } from "@sveltejs/kit";
+import { YOUTUBE_API_URL, YOUTUBE_API_KEY, YOUTUBE_CHANNEL_ID } from '$env/static/private';
+import { z } from "zod";
+import { YtSearchResultSchema } from "../../../core/model/youtube";
+
+
+
+export const GET = async (req) => {
+    const searchParams = new URLSearchParams(req.url.search);
+    const resultSize = searchParams.get("resultsPerPage") ? Number(searchParams.get("resultsPerPage")) : 10;
+   
+    const apiURL = new URL(YOUTUBE_API_URL);
+    apiURL.searchParams.set("part", "snippet");
+    apiURL.searchParams.set("channelId", YOUTUBE_CHANNEL_ID);
+    apiURL.searchParams.set("key", YOUTUBE_API_KEY);
+    apiURL.searchParams.set("order", "date");
+    apiURL.searchParams.set("type", "video");
+    apiURL.searchParams.set("videoDuration", "long");
+    apiURL.searchParams.set("maxResults", resultSize.toString());
+
+    const youtubeReqResult = await fetch(apiURL);
+    const reqDataResult = YtSearchResultSchema.safeParse(await youtubeReqResult.json());
+    if(!reqDataResult.success){
+        return json({
+            success: false
+        });
+    }
+    const reqData = reqDataResult.data;
+    const videos = reqData.items.map(video => ({
+        id: video.id.videoId,
+        ...video.snippet
+    }));
+    
+
+    return json({
+        resultsPerPage: reqData.pageInfo.resultsPerPage,
+        videos
+    });
+}
