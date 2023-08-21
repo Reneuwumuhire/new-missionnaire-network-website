a96d8e9ecbc76027136caa176dce0c93da8acfeb
ninijosue <ninijosue123@gmail.com>
2023-08-21 12:30:30 +0200

    remove params

--- src/lib/server/firestore.ts
+++ src/lib/server/firestore.ts
@@ -1,7 +1,4 @@
 import Firestore from "@google-cloud/firestore";
 
 // TODO: remove the passed object in prod environment
-export const db = new Firestore.Firestore({
-    projectId: "missionnairenetwork",
-    keyFilename: "/home/josue/projects/.config/application_default_credentials.json"
-});
+export const db = new Firestore.Firestore();
