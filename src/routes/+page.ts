4e9f3e523e92e14d347ccfff561511b5ea0f02cc
Josue <ameer@Josue-MacBook-Pro.local>
2024-02-04 17:30:35 +0200

    pass the right params

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,9 @@ import GetSermonsVideosUsecase from '../middleware/usecases/get-videos-sermons';
 export const load = (async ({ fetch }: any) => {
 	const videosUsecase = new GetSermonsVideosUsecase();
 	const res =  await videosUsecase.execute({
-		videoCount: 10
+		videoCount: 10,
+		type: ["frank"],
+		pageNumber: 1
 	});
 	if(res.isOk) {
 		const value = res.value;
