bf9b3666099a654d1154b144b962e51f17bc2773
BAHATI Justin <bajustone@gmail.com>
2024-05-02 09:10:11 +0200

    fixing my git bad habits :(

--- src/routes/api/audios/+server.ts
+++ src/routes/api/audios/+server.ts
@@ -1,17 +1,33 @@
 import { parseSearchParams } from "@mnlib/lib/utils/url";
 import SearchAudiosUsecase, { SearchAudioUsecaseArgsSchema } from "@mnlib/lib/usecase/search-audios";
 import { json } from "@sveltejs/kit";
+import { ZodError } from "zod";
 
 
 export async function GET({ url }) {
 
-	const params = parseSearchParams(url) as any;
+	try {
+		const params = parseSearchParams(url) as any;
 	const { searchTags } = params;
 	if (searchTags) {
 		params["searchTags"] = searchTags.split(",");
 	}
 	const usecase = new SearchAudiosUsecase();
 	const res = await usecase.execute(SearchAudioUsecaseArgsSchema.parse(params));
-
 	return json(res);
+	} catch (e) {
+		let error = new Error("Invalid parameters");
+		if(e instanceof ZodError) {
+            error = new Error(e.issues.map((i) => `${i.code}: ${i.message}`).join(', '));
+        }
+		else if(e instanceof Error) {
+			error = e;
+		}
+		return json({
+			ok: false,
+			error: error.message
+		});
+	}
+
+	
 }
