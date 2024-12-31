73193c89ad27c1635ec171cd412c758db8799367
Uwumuhire Rene <reneuwumuhire@gmail.com>
2024-12-31 16:14:56 +0200

    chore: fixing issues in showuing videos 3

--- src/lib/server/firestore.ts
+++
@@ -1,7 +0,0 @@
-import Firestore from '@google-cloud/firestore';
-
-// TODO: remove the passed object in prod environment
-export const db = new Firestore.Firestore({
-	projectId: 'missionnairenetwork',
-	keyFilename: './application_default_credentials.json'
-});
